e7729234628d7514812712fe7f10584c5dc317f8
MasterBlaster12 <50322478+MasterBlaster12@users.noreply.github.com>
2019-05-11 14:44:08 +0200

    Update test.py
    Mouseclicks

--- test.py
+++ test.py
@@ -1,1 +1,30 @@
-print("Hello Jelly Boat World")
+## print("Hello Jelly Boat World")
+import win32api, win32con, time
+from pynput.mouse import Button, Controller
+
+def click(x,y):
+    win32api.SetCursorPos((x,y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
+
+mouse = Controller()
+
+
+#while True:
+ #try:
+  #click(200,200)
+  #time.sleep(1)
+ #except KeyboardInterrupt:
+     #exit()
+
+
+while True:
+ try:
+  mouse.position = (200, 200)
+  mouse.press(Button.left)
+  #time.sleep(2)
+  mouse.release(Button.left)
+  mouse.position = (500, 200)
+  mouse.press(Button.left)
+ except KeyboardInterrupt:
+     exit()
